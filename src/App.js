c65b22f8f60f9a0937925442dbf947e18ed20f57
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-03 22:47:08 -0300

    trabalhando com use state e props no formulário

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { useState } from "react";
+import Nome from './components/Nome';
+
 function App() {
-  return (
-    <div >
-			<h1>Projeto React Learn</h1>
-    </div>
-  );
+	const [nome, setNome] = useState('')
+	const [email, setEmail] = useState('')
+	const [idade, setIdade] = useState('')
+	
+	const [user, setUser] = useState({})
+	
+	function handlerRegister(e) {
+		e.preventDefault();
+		alert("Usuário registrado com sucesso")
+		setUser({
+			nome:nome,
+			idade:idade,
+			email:email,
+		})
+	}
+
+	return (
+		<div>
+			<form onSubmit={handlerRegister}>
+				<label>Nome</label>		<br></br>
+				<input type="text" value={nome} onChange={(e)=> setNome(e.target.value)} placeholder="digite seu nome"></input>
+				<br></br>
+
+				<label>Email</label>		<br></br>
+				<input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="digite seu email"></input>
+				<br>
+				</br>
+				<label>Idade</label>		<br></br>
+				<input type="text" value={idade} onChange={(e)=> setIdade(e.target.value)} placeholder="digite sua idade"></input>
+				<br></br>
+				<button type="submit" >Registrar</button>
+			</form>
+			<br></br>
+			<div>
+				<span>Bem vindo {user.nome}</span><br/>
+				<span>idade {user.idade}</span><br/>
+				<span>email {user.email}</span><br/>
+
+			</div>
+		</div>
+		
+	);
 }
 
 export default App;
+
