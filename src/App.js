ea9d03ccd3a6d3af26cd5088c39445f4ca4b578b
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-04-05 17:59:56 -0300

    feat: monitorando lista e salvando no localstorage usando useEffect

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
-import { useState } from "react";
-import Nome from './components/Nome';
+import { useEffect, useState } from "react";
 
 function App() {
-	const [nome, setNome] = useState('')
-	const [email, setEmail] = useState('')
-	const [idade, setIdade] = useState('')
+
+	const [input, setInput] = useState('')
+	const [tarefas, setTarefas] = useState([
+		'Estudar java',
+		'Estudar react'
+	]);
 	
-	const [user, setUser] = useState({})
+	useEffect(()=>{
+		localStorage.setItem('@tarefas', JSON.stringify(tarefas))
+		console.log('Salvando no localStorage:', tarefas);
+	}, [tarefas]);
+	useEffect(()=>{
+		
+		const tarefasStorage= localStorage.getItem('@tarefas');
+		console.log('Carregando do localStorage:', tarefasStorage);
+		if(tarefasStorage){
+			setTarefas(JSON.parse(tarefasStorage))
+			console.log('Carregando do localStorage:', tarefasStorage);
+		}
+		}, []);
+
+	
 	
 	function handlerRegister(e) {
 		e.preventDefault();
-		alert("Usuário registrado com sucesso")
-		setUser({
-			nome:nome,
-			idade:idade,
-			email:email,
-		})
+
+		setTarefas([...tarefas, input]);
+		setInput('');
+		
 	}
 
 	return (
 		<div>
 			<form onSubmit={handlerRegister}>
-				<label>Nome</label>		<br></br>
-				<input type="text" value={nome} onChange={(e)=> setNome(e.target.value)} placeholder="digite seu nome"></input>
+				<label>Nome da tarefa</label>		<br></br>
+				<input type="text" value={input} onChange={(e)=> setInput(e.target.value)} placeholder="digite uma tarefa"></input>
 				<br></br>
 
-				<label>Email</label>		<br></br>
-				<input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="digite seu email"></input>
-				<br>
-				</br>
-				<label>Idade</label>		<br></br>
-				<input type="text" value={idade} onChange={(e)=> setIdade(e.target.value)} placeholder="digite sua idade"></input>
-				<br></br>
 				<button type="submit" >Registrar</button>
 			</form>
 			<br></br>
-			<div>
-				<span>Bem vindo {user.nome}</span><br/>
-				<span>idade {user.idade}</span><br/>
-				<span>email {user.email}</span><br/>
+			<ul>
+				{tarefas.map(tarefa=>(
+					<li key={tarefa}>
+						{tarefa}
+					</li>
+				))}
+				
 
-			</div>
+			</ul>
 		</div>
 		
 	);
